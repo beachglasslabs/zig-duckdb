04640a987f61674cdd7322ae6657d06b09b0b788
Ed Yu <edyu@users.noreply.github.com>
2023-06-23 10:54:28 -0700

    Register as the duck module

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 // Although this function looks imperative, note that its job is to
 // declaratively construct a build graph that will be executed by an external
 // runner.
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     // Standard target options allows the person running `zig build` to choose
     // what target to build for. Here we do not override the defaults, which
     // means any target is allowed, and the default is native. Other options
@@ -14,6 +14,12 @@ pub fn build(b: *std.Build) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
+
+    var duck_module = b.createModule(.{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
+    try b.modules.put(b.dupe("duck"), duck_module);
 
     const lib = b.addStaticLibrary(.{
         .name = "duckdb",
@@ -29,9 +35,9 @@ pub fn build(b: *std.Build) void {
     lib.linkSystemLibraryName("duckdb");
     b.installLibFile("lib/libduckdb.so", "libduckdb.so");
 
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
+    // This declares intent for the library to be installed into the standard
+    // location when the user invokes the "install" step (the default step when
+    // running `zig build`).
     b.installArtifact(lib);
 
     // Creates a step for unit testing. This only builds the test executable
